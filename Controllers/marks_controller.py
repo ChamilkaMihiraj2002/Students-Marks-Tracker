9c7d8a6aba2608becb424c0e5719b9c92256d33a
Chamilka Mihiraj <120358764+ChamilkaMihiraj2002@users.noreply.github.com>
2025-04-07 21:39:49 +0530

    Update marks_controller.py

--- Controllers/marks_controller.py
+++ Controllers/marks_controller.py
@@ -3,7 +3,7 @@ import os
 
 # Add the project root directory to sys.path
 sys.path.append(os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__)))))
-from Database.models import Mark
+from Models.mark import Mark
 
 class MarksController:
     """Controller to handle marks operations"""
